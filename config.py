6089d99f93ad8f4bf769a79ad8a5ad7ba3760980
kivails <kiramet2011@gmail.com>
2026-09-15 17:14:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 TOKEN ="8655220760:AAGTs6pnWRl9vQAG0IJhrfLDtmohZmXQotE"
-if not TOKEN or TELEGRAM_TOKEN == "8655220760:AAGTs6pnWRl9vQAG0IJhrfLDtmohZmXQotE":
+TELEGRAM_TOKEN == "8655220760:AAGTs6pnWRl9vQAG0IJhrfLDtmohZmXQotE"
+if not TOKEN:
     raise ValueError("Установите TELEGRAM_TOKEN в переменных окружения")
     
 # Стоимости и награды
