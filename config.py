eaa5c4bfc9f34e361b6862e39ecbb38581f31c56
kivails <kiramet2011@gmail.com>
2026-09-17 20:15:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # ========== ОСНОВНОЕ ==========
-TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TOKEN = "8655220760:AAGTs6pnWRl9vQAG0IJhrfLDtmohZmXQotE"
 if not TOKEN:
     raise ValueError("Установите TELEGRAM_TOKEN в переменных окружения")
 
